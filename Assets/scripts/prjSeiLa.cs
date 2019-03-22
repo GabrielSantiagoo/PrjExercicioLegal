c406bdf3b7e408650f9eb8496f4f2d3c1f118a73
unknown <33568@ETECAF.LOCAL>
2019-03-22 17:27:42 -0300

    Exercicio 22/03

---
+++ Assets/scripts/prjSeiLa.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prjSeiLa : MonoBehaviour {
+
+    public int nivel;
+    public int poder;
+    public int atk;
+    int modificador = 1;
+    public bool critico;
+    int dano;
+    public int numero = 0;
+
+	// Use this for initialization
+	void Start () {
+
+        estranho();
+
+	}
+
+    void ThunderPunch()
+    {
+        
+
+        dano = ((((2 * nivel) / 5) + (2 * poder * atk) / 50) + 2) * modificador;
+
+        if (critico)
+        {
+            dano = dano * 2;
+        }
+
+        Debug.Log("dano = " + dano);
+    }
+
+    void estranho()
+    {
+        int resul;
+
+        resul = numero % 2;
+
+        if (resul == 0)
+        {
+            if (numero >= 6 && numero <= 20)
+            {
+                Debug.Log("Estranho");
+            }
+            else if (numero > 20)
+            {
+                Debug.Log("Não Estranho");
+            }
+            else if (numero >= 2 && numero <= 5)
+            {
+                Debug.Log("Não Estranho");
+            }
+
+        }
+        else
+        {
+            Debug.Log("Estranho");
+        }
+    }
+}
